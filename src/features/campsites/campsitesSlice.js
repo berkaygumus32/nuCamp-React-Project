6b787f813b8d95dfdffe8cce9089906a8e8fa89c
Berkay <berkaygumus32@gmail.com>
2022-08-05 00:19:42 -0600

    random campsite button added

---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,12 @@
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import CampsitesList from './CampsitesList';
+
+
+export const selectAllCampsites = () => {
+    return CAMPSITES;
+};
+
+export const selectRandomCampsite = () => {
+    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())]
+}
+
